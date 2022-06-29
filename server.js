1350b4e32669cbdcfb236bdd6af56efacc3de7fc
saidugurlu <ugurlumsaid@gmx.de>
2022-06-29 11:11:06 +0200

    add post finished

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express');
-const { sequelize } = require('./models');
+const {  sequelize, Flashcard } = require('./models');
  
 const app = express();
 const port = 3042;
+app.use(express.json());
  
 app.get('/', (req, res) => {
     res.send('<h1>Flashcard API</h1>');
 });
+
+
+app.post('/flashcards', async (req, res) => {
+    const { category, front, back } = req.body;
+    try {
+        const flashcard = await Flashcard.create({ category, front, back });
+        return res.json(flashcard);
+    } catch (err) {
+        console.log(err);
+        return res.status(500);
+    }
+});
+
  
 app.listen(port, async () => {
     console.log(`Listening on http://localhost:${port}`);
