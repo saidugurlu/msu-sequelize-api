44a27f17aeeda68ce06168f4d48e35ca2564412a
saidugurlu <ugurlumsaid@gmx.de>
2022-06-29 13:20:48 +0200

    cors added

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const {  sequelize, Flashcard } = require('./models');
+const cors = require('cors');
  
 const app = express();
 const port = 3042;
 app.use(express.json());
+app.use(cors());
  
 app.get('/', (req, res) => {
     res.send('<h1>Flashcard API</h1>');
