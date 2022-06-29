1353eeace9bdf330d108397e09ed154226ef28a7
saidugurlu <ugurlumsaid@gmx.de>
2022-06-29 11:02:37 +0200

    added async

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+const { sequelize } = require('./models');
  
 const app = express();
 const port = 3042;
@@ -7,6 +8,7 @@ app.get('/', (req, res) => {
     res.send('<h1>Flashcard API</h1>');
 });
  
-app.listen(port, () => {
+app.listen(port, async () => {
     console.log(`Listening on http://localhost:${port}`);
+    await sequelize.sync();
 });
